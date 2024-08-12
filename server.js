a00839450cfd6f08eb009e821932683481c6fdd0
ThembaMzumara <tmzumara3@gmail.com>
2024-08-12 15:40:38 -0700

    changes to file -- files rad. file written

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const 
     fs = require('fs'),
     xml2js = require('xml-js'),
-    xmlData = fs.readFileSync('HealthDataSample.xml', 'utf-8'),
-    jsonData = xml2js.xml2json(xmlData, {compact: true, spaces: 4});
+    keys = jsonObj => {
 
-    try {
-        fs.writeFileSync('OutputJsonData.json', jsonData, 'utf-8')
-    } catch (error) { 
-        console.log(error)
-    }
-  
-    console.log('Done')   
+        let keys = [];
+        const traverse = obj => {
+            keys.push(...Object.keys(obj))
+
+            for(let key in obj){
+                if (obj.hasOwnProperty(key)) {
+                    if (typeof obj[key] === 'object' && obj[key] !== null && !Array.isArray(obj[key])) {
+                        traverse(obj[key]); 
+                    }
+                }
+            }
+        }
+        
+        traverse(jsonObj)
+        return keys
+    };
+
+try {
+    // const jsonData = xml2js.xml2json( fs.readFileSync('HealthDataSample.xml', 'utf-8'), {compact: true, spaces: 4});
+    const jsonData = xml2js.xml2json( fs.readFileSync('./xmlBits/ComponentA.xml', 'utf-8'), {compact: true, spaces: 4});
+    fs.writeFileSync('OutputJsonData.json', jsonData, 'utf-8');
+    
+    console.log(keys( JSON.parse( fs.readFileSync('OutputJsonData.json', 'utf-8') ) ))       
+} catch (error) { 
+    console.log(error)
+}
+
+console.log('Done')   
