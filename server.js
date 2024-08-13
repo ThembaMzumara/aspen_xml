5cfc4cf92128c07e53083ed722edf8e373f67cf1
ThembaMzumara <tmzumara3@gmail.com>
2024-08-13 14:12:25 -0700

    file changes. added flatten method

--- server.js
+++ server.js
@@ -30,15 +30,39 @@ const
             }
         }
         return result;
+    },
+    extractTables = jsonObj => {
+        let tables = [];
+    
+        function traverse(obj) {
+            for (let key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    if (key === 'table') {
+                        tables.push({ [key]: obj[key] });
+                    }
+    
+                    if (typeof obj[key] === 'object' && obj[key] !== null) {
+                        traverse(obj[key]);
+                    }
+                }
+            }
+        }
+    
+        traverse(jsonObj);
+        return tables;
     };
 
 try {
-    const jsonData = xml2js.xml2json( fs.readFileSync('HealthDataSample.xml', 'utf-8'), {compact: true, spaces: 4});
-    // const jsonData = xml2js.xml2json( fs.readFileSync('./xmlBits/ComponentR.xml', 'utf-8'), {compact: true, spaces: 4});
+    const
+    // jsonData = xml2js.xml2json( fs.readFileSync('HealthDataSample.xml', 'utf-8'), {compact: true, spaces: 4}),
+    jsonData = xml2js.xml2json( fs.readFileSync('./xmlBits/ComponentD.xml', 'utf-8'), {compact: true, spaces: 4}),
+    tableData = extractTables(JSON.parse( fs.readFileSync('OutputJsonData.json', 'utf-8') ));
+
     fs.writeFileSync('OutputJsonData.json', jsonData, 'utf-8');
+    fs.writeFileSync('OutputTableData.js', JSON.stringify(tableData, null, 2));
     
     // console.log(keys(JSON.parse( fs.readFileSync('OutputJsonData.json', 'utf-8') )))       
-    console.log(flatten(JSON.parse( fs.readFileSync('OutputJsonData.json', 'utf-8') )))       
+    // console.log(flatten(JSON.parse( fs.readFileSync('OutputJsonData.json', 'utf-8') )))       
 } catch (error) { 
     console.log(error)
 }
